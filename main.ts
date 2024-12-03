eab582c8f6783bba2cf176f593012994697ece82
danteasc4 <zexovon@gmail.com>
2024-12-03 10:21:18 -0500

    I'm dumb, it was part 1  & 2

--- main.ts
+++ main.ts
@@ -1,10 +1,9 @@
-import { d1 } from "./d1/d1.ts";
-import { d2 } from "./d2/d2.ts";
+import { d1part1, d1part2 } from "./d1/d1.ts";
 
 function main() {
 	//d1();
 	//console.log("---");
-	d2();
+	//d2();
 }
 
 main();
